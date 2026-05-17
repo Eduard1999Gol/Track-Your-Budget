c9ade2a105b3e4604af72301b6acc1ff1a3c0fe1
Eduard Gohl <edgol199@gmail.com>
2026-05-17 22:31:26 +0200

    remove secrets

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +82,7 @@ SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'APPS': [{
             'client_id': '[REMOVED]',
-            'secret': '[REMOVED]',
+            'secret': os.getenv('GOOGLE_SECRET'),
             'key': ''
         }],
         'SCOPE': ['profile', 'email'],
