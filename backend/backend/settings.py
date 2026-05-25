a4f1c79833bdc4f700e3b7ff69da388e6ccc4232
Eduard Gohl <edgol199@gmail.com>
2026-05-25 13:03:02 +0200

    set expiration to 1 minute to check it works

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,7 +65,7 @@ REST_AUTH_TOKEN_MODEL = None
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
@@ -113,11 +113,6 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-
-CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
